058fda412d003682d89aa685f55e7cd663f94c5f
Freemir <werattest@gmail.com>
2019-09-15 01:26:03 +0600

    Доделал задание на сообразительность.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String [] names = {"Ленин", "Сталин", "Малеников"};
+        String[] names = {"Ленин", "Сталин", "Малеников"};
 
-        for (int swch = 0; swch < names.length; swch++) {
-            switch (swch) {
+        for (int i = 0; i < names.length; i++) {
+            switch (i) {
                 case 0:
                     System.out.println("Доброе утро " + names[0]);
                     break;
@@ -16,6 +18,26 @@ public class Main {
                 case 2:
                     System.out.println("Добрый вечер " + names[2]);
                     break;
+            }
+        }
+
+        System.out.println();
+
+        String[] namesCopy = Arrays.copyOf(names, names.length + 1);
+        namesCopy[3] = "Хрущёв";
+        for (int i = 0; i < namesCopy.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Доброе утро " + namesCopy[0]);
+                    break;
+                case 1:
+                    System.out.println("Доброй день " + namesCopy[1]);
+                    break;
+                case 2:
+                    System.out.println("Добрый вечер " + namesCopy[2]);
+                    break;
+                case 3:
+                    System.out.println("Здраствуйте " + namesCopy[3]);
             }
         }
     }
